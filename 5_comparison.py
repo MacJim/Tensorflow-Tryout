a8986b6da5faf32bba3e483359841f8aae5f80e9
Jim Shi <shiyilin2008@gmail.com>
2020-09-25 11:48:03 -0700

    Adds more element-wise comparison tests.

--- 5_comparison.py
+++ 5_comparison.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 # Disable Tensorflow's debug messages.
 # Must be set before importing Tensorflow.
@@ -8,6 +9,9 @@ import tensorflow as tf
 
 
 def test_element_wise_larger_smaller_1():
+    """
+    Element-wise comparisons.
+    """
     a = tf.constant([
         [1, 2],
         [2, 3]
@@ -27,7 +31,57 @@ def test_element_wise_larger_smaller_1():
 
 
 def test_element_wise_larger_smaller_2():
-    pass
+    """
+    Can compare scalar tensors.
+    """
+    a = tf.constant(5)
+    b = tf.constant(6)
+
+    result1 = a > b
+    result2 = tf.math.maximum(a, b)
+
+    print(f"a > b: {result1}, {type(result1)}, {result1.shape}")    # False, <class 'tensorflow.python.framework.ops.EagerTensor'>, ()
+    print(f"tf.math.maximum(a, b): {result2}, {type(result2)}, {result2.shape}")    # 6, <class 'tensorflow.python.framework.ops.EagerTensor'>, ()
 
 
-test_element_wise_larger_smaller_1()
+def test_element_wise_larger_smaller_3():
+    """
+    Cannot compare tensors of different `dtype`s.
+    Does not work even between (`int32`, `int64`) or (`float32`, `float64`).
+    """
+    a = tf.constant([
+        [1, 2],
+        [2, 3]
+    ], dtype=tf.float32)
+    b = tf.constant([
+        [3, 2],
+        [2, 1]
+    ], dtype=tf.int32)
+    c = tf.cast(a, tf.int64)
+    d = tf.cast(b, tf.float64)
+
+    try:
+        result_ab_1 = a > b
+    except:
+        print(f"Cannot compare a and b: exception: {sys.exc_info()}")    # (<class 'tensorflow.python.framework.errors_impl.InvalidArgumentError'>, InvalidArgumentError(), <traceback object at 0x7fd74ef59088>)
+
+    try:
+        result_ab = tf.math.maximum(a, b)
+    except:
+        print(f"Cannot get max of a and b: exception: {sys.exc_info()}")    # (<class 'tensorflow.python.framework.errors_impl.InvalidArgumentError'>, InvalidArgumentError(), <traceback object at 0x7fd74ef59048>)
+
+    try:
+        result_bc = b > c
+    except:
+        print(f"Cannot compare b and c: exception: {sys.exc_info()}")    # (<class 'tensorflow.python.framework.errors_impl.InvalidArgumentError'>, InvalidArgumentError(), <traceback object at 0x7fd74ef59088>)
+
+    try:
+        result_ad = a > d
+    except:
+        print(f"Cannot compare a and d: exception: {sys.exc_info()}")    # (<class 'tensorflow.python.framework.errors_impl.InvalidArgumentError'>, InvalidArgumentError(), <traceback object at 0x7fd74ef59048>)
+
+
+
+# test_element_wise_larger_smaller_1()
+# test_element_wise_larger_smaller_2()
+test_element_wise_larger_smaller_3()
