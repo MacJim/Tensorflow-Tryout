558799b6612c396d9a0bcdfe8fd5cf8842d67a31
Jim Shi <shiyilin2008@gmail.com>
2020-11-24 22:13:41 -0800

    Adds 1 more Keras ResNet softmax test.

--- 19_keras_resnet.py
+++ 19_keras_resnet.py
@@ -23,9 +23,11 @@ def test_resnet50_layers():
         print(layer)
 
 
-def test_last_layer_softmax():
+def test_last_layer_softmax_1():
     """
-    It seems that softmax is applied to the last (top) dense layer.
+    It seems that softmax is applied to the last (top) dense layer (by default).
+
+    Yet I cannot find the softmax layer in the `.summary()`.
     """
     batch_size = 6
 
@@ -52,6 +54,23 @@ def test_last_layer_softmax():
     model22 = keras.applications.ResNet50V2(weights=None, input_shape=RESNET_DEFAULT_INPUT_SHAPE, classes=N_CLASSES)
     output22 = model22(input2)
     print(f"Output 22: min: {tf.reduce_min(output22).numpy()}, max: {tf.reduce_max(output22).numpy()}, mean: {tf.reduce_mean(output22).numpy()}, sum: {tf.reduce_sum(output22).numpy()}")    # Sum is always float(batch_size)
+
+
+def test_last_layer_softmax_2():
+    """
+    Disable the last (top) dense layer's softmax by specifing `classifier_activation=None`.
+    """
+    batch_size = 6
+
+    input1 = tf.random.uniform((batch_size,) + RESNET_DEFAULT_INPUT_SHAPE, minval=0.0, maxval=1.0)
+    model1 = keras.applications.ResNet50(classifier_activation=None)    # Somehow the `classifier_activation` function is undocumented.
+    output1 = model1(input1)
+    print(f"Output 1: min: {tf.reduce_min(output1).numpy()}, max: {tf.reduce_max(output1).numpy()}, mean: {tf.reduce_mean(output1).numpy()}, sum: {tf.reduce_sum(output1).numpy()}")    # Sum is no longer `batch_size`.
+
+    input2 = tf.random.uniform((batch_size,) + RESNET_DEFAULT_INPUT_SHAPE, minval=-1.0, maxval=1.0)
+    model2 = keras.applications.ResNet50V2(classifier_activation=None)
+    output2 = model2(input2)
+    print(f"Output 2: min: {tf.reduce_min(output2).numpy()}, max: {tf.reduce_max(output2).numpy()}, mean: {tf.reduce_mean(output2).numpy()}, sum: {tf.reduce_sum(output2).numpy()}")    # Sum is no longer `batch_size`.
 
 
 # MARK: - Write summary to file
@@ -102,7 +121,8 @@ if (__name__ == "__main__"):
     # MARK: Print summary
     # test_resnet50_layers()
 
-    test_last_layer_softmax()
+    # test_last_layer_softmax_1()
+    test_last_layer_softmax_2()
 
     exit(0)
 
